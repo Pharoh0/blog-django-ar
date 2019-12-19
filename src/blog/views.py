9687d6abe4179bbff73d093395ca5563f7971b07
Pahroh0 <essameldinart@gmail.com>
2019-12-18 19:49:09 -0800

    Check before save data

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,85 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import NewComment
 # Create your views here.
 
+posts = [
+    {
+        'title': 'التدوينة الاولي',
+        'content': 'تفاصيل التدوينة الاولي كنص تجريبي',
+        'post_date': '15-11-2019',
+        'author': 'Essam Eldin'
+    },
+    {
+        'title': 'التدوينة الثانية',
+        'content': 'تفاصيل التدوينة الثانية كنص تجريبي',
+        'post_date': '15-11-2019',
+        'author': 'Essam Eldin'
+
+    },
+    {
+        'title': 'التدوينة الثالثة',
+        'content': 'تفاصيل التدوينة الثالثة كنص تجريبي',
+        'post_date': '15-11-2019',
+        'author': 'Essam Eldin'
+    },
+    {
+        'title': 'التدوينة الرابعة',
+        'content': 'تفاصيل التدوينة الرابعة كنص تجريبي',
+        'post_date': '15-11-2019',
+        'author': 'Essam Eldin'
+    }
+
+]
+
+
 def home(request):
-    return render(request, 'blog/index.html', {'title': 'Home'})
+    context = {
+        'title': "الرئيسية",
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'من انا'})
+
+
+def post_detail(request, post_id):
+    # show post
+    post = get_object_or_404(Post, pk=post_id)
+    # Show comment
+    comments = post.comments.filter(active=True)
+    # check before save date from comment form.
+    if request.method == 'POST':
+        comment_form = NewComment(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = NewComment()
+    else:
+        comment_form = NewComment()
+    context = {
+        'title': post,
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form,
+    }
+    return render(request, 'blog/detail.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
